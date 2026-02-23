11c377a311448a548e752495675de50d31f4205c
SMDevJi <78262720+SMDevJi@users.noreply.github.com>
2026-02-24 00:28:49 +0530

    Event details,events page (all partial)

--- client/src/pages/EventDetails.tsx
+++ client/src/pages/EventDetails.tsx
@@ -1,15 +1,130 @@
 import { useParams } from 'react-router-dom'
-import { CiMusicNote1 } from "react-icons/ci";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Loading from '../components/Loading';
+import Error from '../components/Error';
+import type { IEvent } from '../types';
+import { IoMdCall } from "react-icons/io";
+import { MdOutlineMail } from "react-icons/md";
+import { FcConferenceCall } from "react-icons/fc";
+import { FaHeart } from "react-icons/fa";
+import { GiLoveSong } from "react-icons/gi";
+import { ImOffice } from "react-icons/im";
+import { MdEventAvailable } from "react-icons/md";
+
+
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 const EventDetails = () => {
     const { eventId } = useParams()
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState('')
+    const [eventData, setEventData] = useState<IEvent | null>({})
+
+
+
+    useEffect(() => {
+        async function getEvent() {
+            try {
+                setLoading(true)
+                const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/events/${eventId}`)
+                setEventData(res.data)
+                console.log(res.data)
+            } catch (error:any) {
+                console.log(error?.response?.data?.message || error?.message);
+                setError('Failed to fetch event data')
+            } finally {
+                setLoading(false)
+            }
+
+        }
+        getEvent()
+    }, [])
+
+
+    if (error) {
+        return <div className='wrapper min-h-[80vh] px-4'>
+            <Error />
+        </div>
+    }
+
+
+    if (loading) {
+        return <div className='wrapper min-h-[80vh] px-4'>
+            <Loading />
+        </div>
+    }
+
     return (
         <div className="min-h-screen max-w-5xl mx-auto w-full">
-            <div className="banner relative h-50 bg-cover bg-center bg-[url('/travel-bg.png')] rounded-lg">
+            <div
+                className="banner relative h-50 bg-cover bg-center rounded-lg"
+                style={{
+                    backgroundImage: eventData?.image
+                        ? `url(${eventData.image})`
+                        : "none",
+                }}
+            >
                 <div className="event-type rounded-full flex justify-center items-center w-25 h-25 border border-gray-300 bg-amber-100 absolute -bottom-10 left-10">
-                    <CiMusicNote1 size={50} />
+
+                    {eventData?.type == 'conference' &&
+                        <FcConferenceCall size={50} />
+                    }
+                    {eventData?.type == 'wedding' &&
+                        <FaHeart size={50}/>
+                    }
+                    {eventData?.type == 'concert' &&
+                        <GiLoveSong size={50}/>
+                    }
+                    {eventData?.type == 'corporate' &&
+                        <ImOffice size={50}/>
+                    }
+                     {eventData?.type == 'other' &&
+                        <MdEventAvailable size={50}/>
+                    }
                 </div>
 
+            </div>
+
+            <div className="second grid grid-cols-1 sm:grid-cols-2 mt-10 p-5 gap-3">
+                <div className="left  p-5 space-y-5">
+                    <h1 className='text-3xl max-w-xs '>{eventData?.title}</h1>
+                    <h1 className='text-2xl text-gray-600'>{eventData?.description}</h1>
+
+                    <div className="contacts grid grid-cols-1 lg:grid-cols-2 gap-2">
+                        <div className="call flex items-center gap-2">
+                            <IoMdCall size={25} />
+                            <h1 className='text-sm'>9876543210</h1>
+                        </div>
+                        <div className="email flex items-center gap-2">
+                            <MdOutlineMail size={25} />
+                            <h1 className='text-sm'>{eventData?.organizerId?.email}</h1>
+                        </div>
+                    </div>
+
+                    <h1 className='text-3xl '>₹{eventData?.ticketPrice}</h1>
+                    <button
+                        className='bg-orange-400 text-white px-3 py-2 rounded-md cursor-pointer w-full hover:scale-105 transition transform duration-200 font-semibold'
+                    >Book now</button>
+                </div>
+                <div className="right border-3 rounded-lg p-3">
+                    <Tabs defaultValue="about" className="w-full mt-5">
+                        <TabsList className='w-full bg-amber-200 '>
+                            <TabsTrigger value="about" className='cursor-pointer '>About</TabsTrigger>
+                            <TabsTrigger value="sponsor" className='cursor-pointer'>Sponsor</TabsTrigger>
+                            <TabsTrigger value="exhibitor" className='cursor-pointer'>Exhibitor</TabsTrigger>
+                        </TabsList>
+                        <TabsContent value="about" className='p-3'>
+                            <h1 className='font-semibold text-lg'>Start date: <span className='font-normal'>{new Date(eventData?.startDate as Date).toLocaleString('en-US')}</span></h1>
+                            <h1 className='font-semibold text-lg'>End date: <span className='font-normal'>{new Date(eventData?.endDate as Date).toLocaleString('en-US')}</span></h1>
+                            <h1 className='font-semibold text-lg'>Max Capacity: <span className='font-normal'>{eventData?.maxCapacity} Seats</span></h1>
+
+                        </TabsContent>
+                        <TabsContent value="sponsor">Change your password here.</TabsContent>
+                        <TabsContent value="exhibitor">Change your password here.</TabsContent>
+                    </Tabs>
+                </div>
             </div>
 
         </div>
--- client/src/pages/Events.tsx
+++ client/src/pages/Events.tsx
@@ -1,10 +1,59 @@
+import Error from "@/components/Error"
+import Loading from "@/components/Loading"
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 
 const Events = () => {
-  return (
-    <div className="min-h-screen w-full">
-        Events
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState('')
+    const [eventsData, setEventsData] = useState<IEvent[] | null>([])
+
+
+
+    useEffect(() => {
+        async function getEvents() {
+            try {
+                setLoading(true)
+                const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/events`)
+                setEventsData(res.data)
+                console.log(res.data)
+            } catch (error: any) {
+                console.log(error?.response?.data?.message || error?.message);
+                setError('Failed to fetch event data')
+            } finally {
+                setLoading(false)
+            }
+
+        }
+        getEvents()
+    }, [])
+
+
+    if (error) {
+        return <div className='wrapper min-h-[80vh] px-4'>
+            <Error />
         </div>
-  )
+    }
+
+
+    if (loading) {
+        return <div className='wrapper min-h-[80vh] px-4'>
+            <Loading />
+        </div>
+    }
+    return (
+        <div className="min-h-screen w-full flex flex-col max-w-5xl mx-auto p-5">
+            <h1 className="text-3xl text-orange-600 mt-2 text-center">Discover What’s Happening Around You...</h1>
+            <div className="con">
+                <div className="left mt-10 w-[30%] border rounded-lg border-orange-400 p-5">
+                    <h1 className="text-3xl text-center">Filter</h1>
+                    <div className="hr h-px bg-orange-400 mt-2"></div>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default Events
