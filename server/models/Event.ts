9295b8c167606dbf554409ff1a8129db531959cd
Sayan Das <sayandas61668@gmail.com>
2026-02-24 14:37:43 +0530

    minor changes in server

--- server/models/Event.ts
+++ server/models/Event.ts
@@ -15,20 +15,20 @@ export interface IEvent {
 }
 
 const EventSchema: Schema = new Schema({
-  image: { type: String, required: true },
-  organizerId: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  type: {
-    type: String,
-    enum: ["conference", "wedding", "concert", "corporate", "other"],
-    required: true,
-  },
-  startDate: { type: Date, required: true },
-  endDate: { type: Date, required: true },
-  venue: { type: Schema.Types.ObjectId, ref: "Venue", required: true },
-  maxCapacity: { type: Number, required: true },
-  ticketPrice: { type: Number, default: 0 },
+    image: { type: String, required: true },
+    organizerId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    type: {
+      type: String,
+      enum: ["conference", "wedding", "concert", "corporate", "other"],
+      required: true,
+    },
+    startDate: { type: Date, required: true },
+    endDate: { type: Date, required: true },
+    venue: { type: Schema.Types.ObjectId, ref: "Venue", required: true },
+    maxCapacity: { type: Number, required: true },
+    ticketPrice: { type: Number, default: 0 },
 }, { timestamps: true });
 
 const Event = mongoose.models?.event || mongoose.model<IEvent>("Event", EventSchema);
